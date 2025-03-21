9329eda50b48daebb4845cd67dc566c553a863de
Ayan Koley <koleyayan07@gmail.com>
2025-03-21 10:38:01 +0530

    optimized like button

--- src/Components/LikeButton.jsx
+++ src/Components/LikeButton.jsx
@@ -6,49 +6,57 @@ import { IconButton  } from '@mui/material';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { GiSelfLove } from "react-icons/gi";
+import { useNavigate } from 'react-router-dom';
 
 function LikeButton({likes, _id}) {
-    // 2 dbs call 1st for provide likes 2nd for remove likes
     const [like, setLike] = useState(likes);
     const [isLiked, setIsLiked] = useState(false);
     const [loader, setLoader] = useState(false);
     const [error, setError] = useState(null);
     const {status} = useSelector(state => state.authSlice);
-    
-    const handleLikeState = () => {
+    const navigate = useNavigate();
+
+    if(status) {
         
-        if(!status) {
-            setIsLiked(!isLiked);
-        }
+        useEffect(() => {
+            const fetchedLikeStatus = async() => {
+                const likeStatus = await axios.get(`/api/v1/like/status/${_id}`);
+                setIsLiked(likeStatus.data.data.isLiked);
+            }
+            fetchedLikeStatus();
+        }, [_id])
     }
-    useEffect(() => {
-        toggleLikes();
-        if(isLiked) setLike(like+1);
-        else setLike(likes);
-    }, [isLiked])
 
     const toggleLikes = async() => {
+        if(!status) navigate("/");
         setLoader(true)
+        setError(null);
         try {
             const response = await axios.post(`/api/v1//like/toggle/v/${_id}`);
-
-            if(!response) {
-                return setError("Failure on like")
-            } 
-            setLoader(false)
+            if(response.data.message.includes('create')) {
+                console.log("work this progress");
+                setLike(prev => prev + 1);
+                setIsLiked(true)
+            }   else {
+                setLike(prev => prev - 1);
+                setIsLiked(false);
+            }
         } catch (err) {
-            setError(err.message)
-            setLoader(false)
+            setError(err.message);
+        } finally {
+            setLoader(false);
         }
     }
+
   return loader ? (
     <IconButton loading />
   ) : (
-    <div className='flex items-center text-white w-10'>
-        <IconButton onClick={handleLikeState} >
+    <div className='flex items-center text-white w-10 mr-5'>
+        <IconButton onClick={toggleLikes} >
             <GiSelfLove className={`${isLiked ? 'text-red-400' : 'text-white'}`} />
         </IconButton>
-        {/* <p className='!text-black mx-3'>{like}</p> */}
+        <p className='!text-white'>{like}</p>
+        {error && <p className='text-red-400'>{error}</p>}
     </div>
   )
 }
