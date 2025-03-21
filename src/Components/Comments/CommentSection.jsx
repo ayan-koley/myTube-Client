8e2264ec12504be741d19d5cdd973237b11cbb58
Ayan Koley <koleyayan07@gmail.com>
2025-03-21 09:07:17 +0530

    bg color change

--- src/Components/Comments/CommentSection.jsx
+++ src/Components/Comments/CommentSection.jsx
@@ -8,30 +8,17 @@ import { fetchedComment } from '../../store/commentSlice';
 
 
 function CommentSection({videoId}) {
-    const[error, setError] = useState(null);
-    const[loading, setLoading] = useState(false);
-    const {comment} = useSelector(state => state.commentSlice);
+    const {comment, error, loading} = useSelector(state => state.commentSlice);
     const dispatch = useDispatch();
+
     useEffect(() => {
         dispatch(fetchedComment(videoId))
     }, [])
-
-    // const fetchedComments = async() => {
-    //     setLoading(true);
-    //     try {
-    //         const allComments = await axios.get(`/api/v1/comment/${videoId}`)
-    //         console.log("all comments are", allComments);
-    //         if(allComments && allComments.data.message.lenght < 1) {
-    //             setError("Faild to fetched comments");
-    //         }
-    //         else setComments(allComments.data.message[0].data);
-    //     } catch (err) {
-    //         setError(err.message)
-    //     }
-    //     setLoading(false);
-    // }
+    
   return (
-    <div className='!w-full md:w-1/2 rounded bg-gray-200 md:mx-3 my-3 text-gray-900 pb-5'>
+    <div className='!w-full md:w-1/2 rounded md:mx-3 my-3 text-gray-900 pb-5'>
+        {error && <p className='text-red-400 m-2'>{error.message}</p>}
+        {loading && <CircularProgress />}
         <div className='text-2xl pl-5 font-bold'>
             Comments
         </div>
