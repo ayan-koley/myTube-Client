f405736fb9687f0759d4b4cf5284c7798e494736
Ayan Koley <koleyayan07@gmail.com>
2025-03-21 10:37:16 +0530

    modifying history page

--- src/Components/WatchedVideos.jsx
+++ src/Components/WatchedVideos.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import {VideoCard} from './index'
+import { Link } from 'react-router-dom';
 
 function WatchedVideos() {
-    const {status} = useSelector(state => state.authSlice);
     const {watchVideos} = useSelector((state) => state.historySlice);
+
   return watchVideos.length > 0 ? (
     <div>
-        <div className='underline text-3xl px-4 py-4'>HISTORY</div>
+        <div className='underline text-white text-center text-3xl px-4 py-4'>HISTORY</div>
+       <div className="grid lg-2:grid-cols-4 md:grid-cols-3 md-2:grid-cols-2 mx-auto gap-10 mt-8">
        {watchVideos[0].map((item) => (
             <div key={item._id} className="px-5 py-5">
-            <VideoCard avatar={item.owner.avatar.url} thumbnail={item.thumbnail.url} title={item.title} username={item.owner.username} createdAt={item.createdAt} views={item.views} />
+              <Link to={`/video/${item._id}`}>
+                <VideoCard avatar={item.owner.avatar.url} thumbnail={item.thumbnail.url} title={item.title} username={item.owner.username} createdAt={item.createdAt} views={item.views} />
+              </Link>
             </div>
+            
        ))}
+       </div>
     </div>
   ) : (
     <div>
-        <div className='text-4xl flex mt-10 justify-center'>
-            {status ? (<div>
-              Watch history is empty
-            </div>) : (<div>
-              Unauthorized Request
-            </div>)}
+        <div className='text-4xl flex mt-10 justify-center'><div>
+          Watch history is empty
         </div>
+      </div>
     </div>
   )
 }
