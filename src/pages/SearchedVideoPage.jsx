e2efb47a7b75d3c75110700a6266d8cad8ea3baa
Ayan Koley <koleyayan07@gmail.com>
2025-03-21 10:41:12 +0530

    add a new page of searcing result of videos

---
+++ src/pages/SearchedVideoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SearchedVideo } from '../Components'
+
+function SearchedVideoPage() {
+  return (
+    <div>
+        <SearchedVideo />
+    </div>
+  )
+}
+
+export default SearchedVideoPage
