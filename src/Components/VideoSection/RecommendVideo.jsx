57f2a5fb9b73ccc7d16f25afd8ad116a785cdc16
Ayan Koley <koleyayan07@gmail.com>
2025-03-21 10:39:50 +0530

    on the way

--- src/Components/VideoSection/RecommendVideo.jsx
+++ src/Components/VideoSection/RecommendVideo.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VideoCard from '../VideoCard';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function RecommendVideo() {
+function RecommendVideo({videoId}) {
+  const [videos, setVideos] = useState([]);
     const { searchedVideos, query } = useSelector((state) => state.videoSlice);
+    useEffect(() => {
+      // setVideos(searchedVideos[query].filter((item) => item._id !== videoId));
+      let result = searchedVideos[query].filter((item) => item._id !== videoId);
+      setVideos(result);
+    }, [])
+    console.log(setVideos);
   return (
     <div className='border-gray-400 border-2 rounded-2xl p-10 m-5'>
         <div>
             <div className='underline text-gray-300 font-bold text-2xl p-5'>Similar Type Videos</div>
         </div>
-        {searchedVideos[query]?.map((item) => (
+        {videos?.map((item) => (
+          
             <div key={item._id} className="flex justify-center mt-5">
                 <Link to={`/video/${item._id}`}>
                     <VideoCard
